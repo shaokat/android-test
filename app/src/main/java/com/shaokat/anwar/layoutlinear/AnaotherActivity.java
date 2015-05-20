47fe88f7038519d1d17a930dbb1ee0a5a30f6f9d
shaokat <shaokatcse@gmail.com>
2015-05-20 12:12:21 +0600

    add new file

---
+++ app/src/main/java/com/shaokat/anwar/layoutlinear/AnaotherActivity.java
@@ -0,0 +1,15 @@
+package com.shaokat.anwar.layoutlinear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ANWAR on 5/18/2015.
+ */
+public class AnaotherActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_another);
+    }
+}
